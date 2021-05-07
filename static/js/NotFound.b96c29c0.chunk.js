3f1f5551de0df3ec1dfcd0aba70bde408183eed3
Klirik <dasoftweb@gmail.com>
2021-05-07 23:14:57 +0300

    Updates

---
+++ static/js/NotFound.b96c29c0.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-04-movies"]=this["webpackJsonpgoit-react-hw-04-movies"]||[]).push([[5],{100:function(t,e,n){"use strict";n.r(e);var s=n(1);e.default=function(){return Object(s.jsx)("div",{className:"Content",children:Object(s.jsx)("h1",{className:"Content-title",children:"404 - Page not found"})})}}}]);
+//# sourceMappingURL=NotFound.b96c29c0.chunk.js.map
